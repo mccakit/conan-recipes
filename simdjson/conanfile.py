9a02e2682092eab0381eb7401c9c3a26eb01e53a
mccakit <mccakit09@gmail.com>
2026-03-10 02:46:44 +0300

    fmt, graaf and simdjson

---
+++ simdjson/conanfile.py
@@ -0,0 +1,39 @@
+from conan import ConanFile
+import os
+import subprocess
+
+
+class simdjson(ConanFile):
+    name = "simdjson"
+    version = "master"
+    settings = "os", "arch", "compiler", "build_type"
+    def requirements(self):
+        pass
+
+    def source(self):
+        subprocess.run(
+            f'bash -c "git clone --recurse-submodules --shallow-submodules --depth 1 git@github.com:simdjson/simdjson.git -b {self.version}"',
+            shell=True,
+            check=True,
+        )
+
+    def build(self):
+        cmake_toolchain = self.conf.get("user.mccakit:cmake", None)
+        os.chdir("simdjson")
+        pkgconf_path = ":".join(
+            os.path.join(dep.package_folder, "lib", "pkgconfig")
+            for dep in self.dependencies.values()
+        )
+        os.environ["PKG_CONFIG_LIBDIR"] = pkgconf_path
+        cmake_prefix_path = ";".join(
+            dep.package_folder for dep in self.dependencies.values()
+        )
+        subprocess.run(
+            f'bash -c "cmake -B build -G Ninja -DCMAKE_PREFIX_PATH=\\"{cmake_prefix_path}\\" -DCMAKE_TOOLCHAIN_FILE={cmake_toolchain} -DCMAKE_INSTALL_PREFIX={self.package_folder} -DSIMDJSON_DISABLE_DEPRECATED_API=ON"',
+            shell=True,
+            check=True,
+        )
+        subprocess.run(
+            f'bash -c "cmake --build build --parallel"', shell=True, check=True
+        )
+        subprocess.run(f'bash -c "cmake --install build"', shell=True, check=True)
