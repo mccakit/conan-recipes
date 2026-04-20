26663ad96b861760ebb7feda3582290e91308c28
mccakit <mccakit09@gmail.com>
2026-04-20 17:06:36 +0300

    rocksdb

---
+++ gflags/conanfile.py
@@ -0,0 +1,37 @@
+from conan import ConanFile
+import os
+import subprocess
+
+
+class gflags(ConanFile):
+    name = "gflags"
+    version = "main"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def source(self):
+        subprocess.run(
+            f'bash -c "git clone --recurse-submodules --shallow-submodules --depth 1 git@github.com:gflags/gflags.git -b {self.version}"',
+            shell=True,
+            check=True,
+        )
+
+    def build(self):
+        cmake_toolchain = self.conf.get("user.mccakit:cmake", None)
+        os.chdir("gflags")
+        pkgconf_path = ":".join(
+            os.path.join(dep.package_folder, "lib", "pkgconfig")
+            for dep in self.dependencies.values()
+        )
+        os.environ["PKG_CONFIG_LIBDIR"] = pkgconf_path
+        cmake_prefix_path = ";".join(
+            dep.package_folder for dep in self.dependencies.values()
+        )
+        subprocess.run(
+            f'bash -c "cmake -B build -G Ninja -DCMAKE_PREFIX_PATH=\\"{cmake_prefix_path}\\" -DCMAKE_TOOLCHAIN_FILE={cmake_toolchain} -DCMAKE_INSTALL_PREFIX={self.package_folder}"',
+            shell=True,
+            check=True,
+        )
+        subprocess.run(
+            f'bash -c "cmake --build build --parallel"', shell=True, check=True
+        )
+        subprocess.run(f'bash -c "cmake --install build"', shell=True, check=True)
