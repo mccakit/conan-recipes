69d0154c7ca06d862e5c5e882d395f37816ec161
mccakit <mccakit09@gmail.com>
2026-05-12 21:39:02 +0300

    update

---
+++ doctest/conanfile.py
@@ -0,0 +1,41 @@
+import os
+import subprocess
+
+from conan import ConanFile
+
+
+class doctest(ConanFile):
+    name = "doctest"
+    version = "v2.5.2"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def requirements(self):
+        pass
+
+    def source(self):
+        subprocess.run(
+            f'bash -c "git clone --recurse-submodules --shallow-submodules --depth 1 git@github.com:doctest/doctest.git -b {self.version}"',
+            shell=True,
+            check=True,
+        )
+
+    def build(self):
+        cmake_toolchain = self.conf.get("user.mccakit:cmake", None)
+        os.chdir("doctest")
+        pkgconf_path = ":".join(
+            os.path.join(dep.package_folder, "lib", "pkgconfig")
+            for dep in self.dependencies.values()
+        )
+        os.environ["PKG_CONFIG_LIBDIR"] = pkgconf_path
+        cmake_prefix_path = ";".join(
+            dep.package_folder for dep in self.dependencies.values()
+        )
+        subprocess.run(
+            f'bash -c "cmake -B build -G Ninja -DCMAKE_PREFIX_PATH=\\"{cmake_prefix_path}\\" -DCMAKE_TOOLCHAIN_FILE={cmake_toolchain} -DCMAKE_INSTALL_PREFIX={self.package_folder} -DDOCTEST_WITH_TESTS=OFF -DDOCTEST_USE_STD_HEADERS=ON"',
+            shell=True,
+            check=True,
+        )
+        subprocess.run(
+            f'bash -c "cmake --build build --parallel"', shell=True, check=True
+        )
+        subprocess.run(f'bash -c "cmake --install build"', shell=True, check=True)
